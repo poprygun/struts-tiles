119a66e3f11a4ac23423cbcbfff79054067d67d3
Alex Shumilov <ashumilov@pivotal.io>
2018-03-15 07:19:18 -0400

    Move configuration from web.xml to configuration file.

--- src/main/java/io/microsamples/strutstiles/configuration/StrutsConfig.java
+++ src/main/java/io/microsamples/strutstiles/configuration/StrutsConfig.java
@@ -2,7 +2,9 @@ package io.microsamples.strutstiles.configuration;
 
 
 import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
+import org.apache.struts2.tiles.StrutsTilesListener;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,9 +16,16 @@ public class StrutsConfig {
     public FilterRegistrationBean someFilterRegistration() {
         FilterRegistrationBean registration = new FilterRegistrationBean();
         registration.setFilter(new StrutsPrepareAndExecuteFilter());
-        registration.addUrlPatterns("*.action");
+        registration.addUrlPatterns("/*");
         registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.FORWARD);
         registration.setName("StrutsPrepareAndExecuteFilter");
         return registration;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean tilesListenerRegistration(){
+        ServletListenerRegistrationBean listenerRegistrationBean = new ServletListenerRegistrationBean();
+        listenerRegistrationBean.setListener(new StrutsTilesListener());
+        return listenerRegistrationBean;
     }
 }
